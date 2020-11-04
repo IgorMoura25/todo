c8a2e2dff1c5b4ad013727f5d655f581a2a5831f
Everton Brzozowy Alves <everton.brzozowy.alves@gmail.com>
2020-11-04 00:53:50 -0300

    Todo.Data, com Dapper conectando no banco, injeção de dependência configurada na API e implementando interface

--- todo.api/Controllers/NoteController.cs
+++ todo.api/Controllers/NoteController.cs
@@ -4,7 +4,8 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
-using Todo.Models.Note;
+using Todo.Data.Interfaces;
+using Todo.Models;
 
 namespace Todo.Api.Controllers
 {
@@ -12,14 +13,27 @@ namespace Todo.Api.Controllers
     [Route("note")]
     public class NoteController : ControllerBase
     {
+        private INoteDao _noteDao;
+        public NoteController(INoteDao noteDao)
+        {
+            _noteDao = noteDao;
+        }
+
         [HttpGet("types")]
         public List<ListNoteTypesResponseModel> ListNoteTypes()
         {
             //TODO: Remover hardcode
-            return new List<ListNoteTypesResponseModel>() { 
+            return new List<ListNoteTypesResponseModel>() {
                 new ListNoteTypesResponseModel() { NoteTypeId = 1, Name = "Lista" },
                 new ListNoteTypesResponseModel() { NoteTypeId = 2, Name = "Texto" },
             };
         }
+        [HttpGet("notes")]
+        public IEnumerable<Note> ListNotes()
+        {
+            var notes = _noteDao.GetAllNotes();
+            return notes;
+        }
+
     }
 }
