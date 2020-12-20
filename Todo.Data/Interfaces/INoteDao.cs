a0503a48c14f1c005e08ab26dafb2ccd4ead12c9
Everton Brzozowy Alves <everton.brzozowy.alves@gmail.com>
2020-12-20 13:03:20 -0300

    refactor: removing unnecessary lines from code.

--- Todo.Data/Interfaces/INoteDao.cs
+++ Todo.Data/Interfaces/INoteDao.cs
@@ -1,6 +1,4 @@
-using System;
 using System.Collections.Generic;
-using System.Text;
 using Todo.Models.Entities.Note;
 
 namespace Todo.Data.Interfaces
--- Todo.Models/Entities/Note/GetNoteByIdRequestModel.cs
+++ Todo.Models/Entities/Note/GetNoteByIdRequestModel.cs
@@ -1,6 +1,3 @@
-using System;
-using System.Collections.Generic;
-using System.Text;
 using Todo.Models.Base;
 
 namespace Todo.Models.Entities.Note
